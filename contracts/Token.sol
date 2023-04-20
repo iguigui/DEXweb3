df87af526322dd48d271f046de2f1c6c42d7db19
iguigui <andrewthebarr@gmail.com>
2023-04-19 22:35:29 -0400

    workings

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,9 +9,13 @@ contract Token{
   uint256 public decimals = 18; 
   uint256 public totalSupply; //1,000,000 * 10^18; 
 
+  mapping(address => uint256) public balanceOf;
+
   constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
     name = _name;
     symbol = _symbol;
     totalSupply = _totalSupply * (10**decimals);
+    //assign all the tokens to the deployer of the contract
+    balanceOf[msg.sender] = totalSupply;
   }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,32 +6,43 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("Token", () => {
-  let token;
+let token, accounts, deployer;
 
-  beforeEach(async () => {
-    const Token = await ethers.getContractFactory("Token");
-    token = await Token.deploy("Andrew Token", "ATOK", "1000000");
+beforeEach(async () => {
+  const Token = await ethers.getContractFactory("Token");
+  token = await Token.deploy("Andrew Token", "ATOK", "1000000");
 
-    // let accounts = Token.getSigners();
-    // let deployer = accounts[0];
-  });
+  accounts = await ethers.getSigners();
+  deployer = accounts[0];
+});
+
+describe("Deployment", () => {
+  const name = "Andrew Token";
+  const symbol = "ATOK";
+  const decimals = "18";
+  const totalSupply = tokens("1000000");
 
   it("Has correct name", async () => {
     //Check that name is correct
-    expect(await token.name()).to.equal("Andrew Token");
+    expect(await token.name()).to.equal(name);
   });
 
   it("Has correct symbol", async () => {
     //Check that symbol is correct
-    expect(await token.symbol()).to.equal("ATOK");
+    expect(await token.symbol()).to.equal(symbol);
   });
   it("Has correct decimals", async () => {
     //Check that symbol is correct
-    expect(await token.decimals()).to.equal("18");
+    expect(await token.decimals()).to.equal(decimals);
   });
   it("Has correct total supply", async () => {
     //Check that symbol is correct
-    expect(await token.totalSupply()).to.equal(tokens("1000000"));
+    expect(await token.totalSupply()).to.equal(totalSupply);
+  });
+  it("assigns total supply to deployer", async () => {
+    console.log(deployer.address);
+    expect(await token.balanceOf(deployer.address)).to.equal(
+      await token.totalSupply()
+    );
   });
 });
